a582c11cf243159af322e6f753e56bfd3d2a5782
qiyomovalaziz-art <qiyomovalaziz@gmail.com>
2025-11-08 21:32:59 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-BOT_TOKEN = "BOT_TOKENINGIZNI_BU_YERGA_QOYING"
+BOT_TOKEN = "8347087773:AAFuMVzJIdPxg-iujpOXLw_Zb-CnBG5PcTw"
 ADMIN_ID = 123456789  # o'z Telegram ID raqamingizni kiriting
