ebb1f48b10546b407305a4551d41cb633b10122e
qiyomovalaziz-art <qiyomovalaziz@gmail.com>
2025-11-08 21:40:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # config.py
-BOT_TOKEN = "8347087773:AAFuMVzJIdPxg-iujpOXLw_Zb-CnBG5PcTw"
+BOT_TOKEN =
+"8347087773:AAFuMVzJIdPxg-iujpOXLw_Zb-CnBG5PcTw"
 ADMIN_ID = 123456789  # o'z Telegram ID raqamingizni kiriting
